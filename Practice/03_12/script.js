c0c8dfe6a77edb02b9557f2619d7488de933310a
willchan-117 <willchan117@outlook.com>
2025-11-18 16:48:59 +0000

    practices

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,17 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+class backpack{
+constructor(name, volume) {
+    this.name = name;
+    this.volume = volume;
+    }
+}
+
+ToggleLid();{
+    console.log('Lid toggled');
+}
+
+const myBackpack = new backpack('Hiking Pack', 30);
+myBackpack.toggleLid(); // Output: Lid toggled
+
